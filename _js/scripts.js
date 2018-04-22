0728ddceeeae78e14aad4e7937df7e3eb9358346
Sidorela Uku <uku.sidorela@gmail.com>
2018-04-22 23:06:34 +0200

    Update scripts.js

--- _js/scripts.js
+++ _js/scripts.js
@@ -262,7 +262,7 @@ $( "#contactForm" ).submit( function( e ) {
     $.ajax( {
 
         // Change the email address here:
-        url: "https://formspree.io/uku.sidorela@.com",
+        url: "https://formspree.io/uku.sidorela@gmail.com",
         method: "POST",
         data: $( this ).serialize(),
         dataType: "json",
